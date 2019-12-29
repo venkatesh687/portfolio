1da1c60e5e1ce653bee981bc5a4d0953032d5408
Your venkatesh687 <venkateshsri687@gmail.com>
2019-12-29 20:18:28 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FrontpageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
